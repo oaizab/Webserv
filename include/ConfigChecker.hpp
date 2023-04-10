dfe809a5cdc29eaca03abe5c481daf1aea3558dc
Hamza Haddani <hhamza@student.1337.ma>
2023-04-10 12:31:24 +0000

    chore(ConfigChecker): Re-organize class prototype

--- include/ConfigChecker.hpp
+++ include/ConfigChecker.hpp
@@ -28,22 +28,25 @@ class ConfigChecker
 		static bool validateErrorCode(const std::string &code);
 		static bool validateErrorPages(const std::string &errorPages);
 
-		static void validateServerBlock(std::ifstream &fin);
+		// One-line directive validators
+		void validateHostDirective(const std::vector<std::string> &tokens);
+		void validatePortDirective(const std::vector<std::string> &tokens);
+		void validateServerNameDirective(const std::vector<std::string> &tokens);
+		void validateErrorPagesDirective(const std::vector<std::string> &tokens, std::ifstream &fin);
+		void validateClientMaxBodySizeDirective(const std::vector<std::string> &tokens);
 
-		static void validateHostDirective(const std::vector<std::string> &tokens);
-		static void validatePortDirective(const std::vector<std::string> &tokens);
-		static void validateServerNameDirective(const std::vector<std::string> &tokens);
-		static void validateErrorPagesDirective(const std::vector<std::string> &tokens, std::ifstream &fin);
-		static void validateClientMaxBodySizeDirective(const std::vector<std::string> &tokens);
+		// TODO(ConfigChecker): Implement these One-line directive validators
+		void validateAllowedMethodsDirective(const std::vector<std::string> &tokens);
+		void validateRedirectDirective(const std::vector<std::string> &tokens);
+		void validateRootDirective(const std::vector<std::string> &tokens);
+		void validateAutoindexDirective(const std::vector<std::string> &tokens);
+		void validateIndexDirective(const std::vector<std::string> &tokens);
+		void validateUploadDirective(const std::vector<std::string> &tokens);
 
-		static void validateLocationDirective(const std::vector<std::string> &tokens, std::ifstream &fin);
-		static void validateAllowedMethodsDirective(const std::vector<std::string> &tokens);
-		static void validateRedirectDirective(const std::vector<std::string> &tokens);
-		static void validateRootDirective(const std::vector<std::string> &tokens);
-		static void validateAutoindexDirective(const std::vector<std::string> &tokens);
-		static void validateIndexDirective(const std::vector<std::string> &tokens);
-		static void validateCgiDirective(const std::vector<std::string> &tokens);
-		static void validateUploadDirective(const std::vector<std::string> &tokens);
+		// Block directive validators
+		void validateServerBlock(std::ifstream &fin);
+		void validateLocationDirective(const std::vector<std::string> &tokens, std::ifstream &fin);
+		void validateCgiDirective(const std::vector<std::string> &tokens);
 
 		// Methods
 		void validateConfigFile();
