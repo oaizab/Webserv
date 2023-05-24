254fc5b4b84f1ac18af8710ad3d24b2361a0f33d
Hamza Haddani <hhamza@student.1337.ma>
2023-05-24 14:57:01 +0100

    feat: Add python cgi

---
+++ html/python/index.py
@@ -0,0 +1,11 @@
+print("Content-type: text/html\r\n\r\n")
+
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head>")
+print("<title>CGI Python Test</title>")
+print("</head>")
+print("<body>")
+print("<h2>Hello World! This is my first CGI Python program</h2>")
+print("</body>")
+print("</html>")
